8e78dc63a44ec1196dbd05bb080bda1aec8a22be
Dhanisha <dhanisha522292@gmail.com>
2025-08-23 01:20:47 +0200

    stack

---
+++ stacks.py
@@ -0,0 +1,15 @@
+''' Stack
+--> Stacks are Linear Data Structures.
+--> Each stack is assumed to be inside a container with only its top side open, where only the top element of stack
+    can be accessed at a time.
+--> The working principle of stack is 'Last In First Out' (LIFO).
+--> Operations on stack: A) Push B) Pop C) Peek D) is_empty E) size.
+
+--> Implementation of Stack:- 
+1. using List
+2. by extending a list class
+3. 
+  
+'''
+
+#Implementation of stack using list:-
