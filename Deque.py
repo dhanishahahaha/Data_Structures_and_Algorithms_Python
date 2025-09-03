90e85c2bd3e8593672fab3b6b5c3684d5c44e7fe
Dhanisha <dhanisha522292@gmail.com>
2025-09-03 17:10:52 +0200

    deque using dll

--- Deque.py
+++ Deque.py
@@ -35,6 +35,19 @@ Ans) To be able to traverse in both directions due to insertion and deletion on 
 
 #1. Implementation of Deque using List class
 
+'''
+--> make a Deque class to use the concept of deque, define an init method and make an empty list.
+--> Define a method is_empty() to check if the deque is empty or not.
+--> Define a method insert_front() to add an item to the front of the deque.
+--> Define a method insert_rare() to add an item to the rare of the deque.
+--> Define a method delete_front() to delete an item at the front of the deque.
+--> Define a method delete_rare() to delete an item at the rare of the deque.
+--> Define a method get_front() to view an item at the front of the deque.
+--> Define a method get_rare() view an item at the rare of the deque.
+--> Define a method size() to return the number of items in the deque.
+--> Define a method display() to view all the elements of the deque.
+'''
+
 class Deque:
     def __init__(self):
         self.items=[]
@@ -88,3 +101,125 @@ print("Number of elements are: ",q1.size())
 q1.display()
 print("Front element is ",q1.get_front())
 print("Rare element is ",q1.get_rare())    
+
+
+
+#2. Implementation of Deque using Doubly Linked List 
+
+'''
+--> make a Node class to initialize prev, item, next
+--> make a Deque class to use the concept of deque, define an init method and make an empty list.
+--> Define a method is_empty() to check if the deque is empty or not.
+--> Define a method insert_front() to add an item to the front of the deque.
+--> Define a method insert_rare() to add an item to the rare of the deque.
+--> Define a method delete_front() to delete an item at the front of the deque.
+--> Define a method delete_rare() to delete an item at the rare of the deque.
+--> Define a method get_front() to view an item at the front of the deque.
+--> Define a method get_rare() view an item at the rare of the deque.
+--> Define a method size() to return the number of items in the deque.
+--> Define a method display() to view all the elements of the deque.
+'''
+
+class Node:
+    def __init__(self,prev=None,item=None,next=None):
+        self.item=item
+        self.prev=prev
+        self.next=next
+
+class Deque:
+    def __init__(self):
+        self.front=None
+        self.rear=None
+        self.item_count=0
+
+    def is_empty(self):
+        return self.item_count==0
+
+    def insert_front(self,data):
+        n=Node(None,data,self.front) #prev,item,next
+        if self.is_empty():
+            self.front=n
+            self.rear=n
+        else:
+            self.front.prev=n
+            self.front=n
+        self.item_count+=1
+    def insert_rear(self,data):
+        n=Node(self.rear,data,None) #prev,item,next
+        if self.is_empty():
+            self.front=n
+            self.rear=n
+        else:
+            self.rear.next=n
+            self.rear=n
+        self.item_count+=1
+    def delete_front(self):
+        if self.is_empty():
+            raise IndexError("Deque Underflow")
+        if self.front==self.rear:
+            self.front=None
+            self.rear=None
+        else:
+            self.front=self.front.next
+            self.front.prev=None
+        self.item_count-=1
+
+    def delete_rear(self):
+        if self.is_empty():
+            raise IndexError("Deque Underflow")
+        if self.front==self.rear:
+            self.front=None
+            self.rear=None
+        else:
+            self.rear=self.rear.prev
+            self.rear.next=None
+        self.item_count-=1
+
+    def get_front(self):
+        if self.is_empty():
+            raise IndexError("Deque Underflow")
+        else:
+            return self.front.item
+    
+    def get_rear(self):
+        if self.is_empty():
+            raise IndexError("Deque Underflow")
+        else:
+            return self.rear.item
+    
+    def size(self):
+        return self.item_count
+    
+    def display(self):
+        temp=self.front
+        if self.is_empty():
+            raise IndexError("Deque Underflow")
+        else:
+            while temp is not None:
+                print(temp.item, end= " ")
+                temp=temp.next
+            print() #new line clean output
+
+d2=Deque()
+d2.insert_front(1)
+d2.insert_front(2)
+d2.insert_front(3)
+d2.insert_front(4)
+d2.insert_front(5)
+d2.insert_rear(6)
+d2.insert_rear(7)
+d2.insert_rear(8)
+d2.insert_rear(9)
+d2.insert_rear(10)
+print("The front element is :",d2.get_front())
+print("The rear element is :",d2.get_rear())
+print("Number of items in the deque: ",d2.item_count)
+print("Elements of the deque: ")
+d2.display()
+d2.delete_front()
+d2.delete_rear()
+print("The front element is :",d2.get_front())
+print("The rear element is :",d2.get_rear())
+print("Number of items in the deque: ",d2.item_count)
+print("Elements of the deque: ")
+d2.display()
