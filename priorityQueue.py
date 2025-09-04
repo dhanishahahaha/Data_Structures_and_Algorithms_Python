fb81a527dfa0508d6846d6ce8697e29da6bdf139
Dhanisha <dhanisha522292@gmail.com>
2025-09-04 15:54:31 +0200

    PQ

---
+++ priorityQueue.py
@@ -0,0 +1,23 @@
+'''
+Priority Queue 
+
+--> A collection of elements such that each element has been assigned a priority number.
+--> The order of the elements are deleted and processed comes from the following rules:-
+a) The element of the higher priority is processed before the element of the lower priority.
+b) If two elements are of same priority then they are processed according to the order in which they were added 
+   in the priority queue.
+
+--> Elements can be inserted and deleted anywhere in the PQ.
+   
+--> Operation in the priority queue:-
+a) push
+b) pop
+c) is_empty
+d) size
+
+--> Implementation of the priority queue:-
+a) using the Linked List Concept
+b) using Lists
+c) using Heap
+
+'''
