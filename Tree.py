63f859a5ac3f7300329233bf5746c67e22fbae75
Dhanisha <dhanisha522292@gmail.com>
2025-09-09 22:34:23 +0200

    Binary Tree

--- Tree.py
+++ Tree.py
@@ -16,4 +16,19 @@
     f) Path - Connection of nodes excluding the leaf node.
     g) Branch - Connection of nodes including the leaf node.
     h) Height/Depth of the tree - No. of the nodes in the largest branch OR Total no. of generations.
+
+    
+Binary Tree - A binary tree is defined as a finite set of elements called nodes such that (a) tree is empty or
+(b) contains a distinguished node R called the root of the tree(T) and the remaining nodes of T form an 
+ordered pair of disjoint binary trees T1 and T2.
+
+--> Any node in the Binary tree has 0,1,or 2 child nodes.
+
+--> types of Binary Tree: a) Complete Binary Tree
+                          b) Almost Complete Binary Tree
+                          c) Strict Binary Tree
+
+--> Representation of a Binary Tree: a) Array Representation
+                                     b) Linked Representation (by default)
+
 '''
