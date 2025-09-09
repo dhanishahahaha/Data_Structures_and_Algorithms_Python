970284318de6bc896b5cfa8dc2e5540da7c4ed0c
Dhanisha <dhanisha522292@gmail.com>
2025-09-09 17:27:10 +0200

    recursion

--- recursion.py
+++ recursion.py
@@ -13,7 +13,7 @@ def f1(n):
     return n+f(n-1) #recursive case
     
 '''
-
+#print the first N natural numbers using recursion.
 def printN(n):
     if n>=0:
         printN(n-1)
@@ -21,6 +21,7 @@ def printN(n):
 
 printN(10)
 
+#print the first N natural numbers in reverse using recursion.
 def printNreverse(n):
     if n>=0:
         print(n, end=" ")
@@ -28,6 +29,7 @@ def printNreverse(n):
 
 printNreverse(10)
 
+#print the first N odd natural numbers using recursion.
 def printNodd(n):
     if n>0:
         printNodd(n-1)
@@ -35,6 +37,7 @@ def printNodd(n):
 
 printNodd(10)
 
+#print the first N even natural numbers using recursion.
 def printNeven(n):
     if n>0:
         printNeven(n-1)
@@ -42,6 +45,7 @@ def printNeven(n):
 
 printNeven(10)
 
+#print the first N natural numbers in reverse using recursion.
 def printOddReverese(n):
     if n>0:
         print(2*n-1, end=" ")
@@ -49,9 +53,50 @@ def printOddReverese(n):
 
 printOddReverese(10)
 
+#print the first N even natural numbers in reverse using recursion.
 def printEvenReverese(n):
     if n>0:
         print(2*n, end=" ")
         printEvenReverese(n-1)
 
 printEvenReverese(10)
+
+#print the sum of first N natural numbers using recursion.
+def SumN(n):
+    if n==1:
+        return 1
+    return n + SumN(n-1)
+
+print("The sum is: ",SumN(10))
+
+#print the sum of first N odd natural numbers using recursion.
+def SumOddN(n):
+    if n==1:
+        return 1
+    return 2*n-1 + SumOddN(n-1)
+
+print("The sum of Odd numbers is :",SumOddN(10))
+
+#print the sum of first N even natural numbers using recursion.
+def SumEvenN(n):
+    if n==1:
+        return 2
+    return 2*n + SumEvenN(n-1)
+
+print("The sum of even numbers is :",SumEvenN(10))
+
+#print the factorial of first N natural numbers using recursion.
+def factorialN(n):
+    if n==0:
+        return 1
+    return n * factorialN(n-1)
+
+print("The factorial of numbers are: ", factorialN(10))
+
+#print the sum of squares of N natural numbers using recursion.
+def squares(n):
+    if n==1:
+        return 1
+    return n**2 + squares(n-1)
+
+print("the sum of square of numbers are: ",squares(5))
