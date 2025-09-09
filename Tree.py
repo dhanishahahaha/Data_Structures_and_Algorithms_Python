6c2b7c0aafa7996a3a36395466eccfc44d6d8873
Dhanisha <dhanisha522292@gmail.com>
2025-09-09 21:43:57 +0200

    tree

---
+++ Tree.py
@@ -0,0 +1,19 @@
+'''Tree
+
+--> A tree is defined as a finite set of one or more data items such that there is a special node called the
+    root node and the remaining nodes are partitioned into n>=0 disjoint subsets of which is itself a tree, 
+    and they are known as Subtrees.
+
+--> A tree is a non-linear and Hirarchical data structure. 
+--> Binary Tree - A tree with a maximum degree of 2
+    
+--> Common terms:-
+    a) Root node - Main node or first node from which all other branches emerge.
+    b) leaf nodes/Terminal nodes - last nodes of the tree with degree zero.
+    c) Degree of nodes - No. of nodes emerging from a particular node.
+    d) Degree of a tree - The highest degree of a node in the tree is the degree of the tree.
+    e) Levels/Generations - Group of nodes from every parent node in the hirarchy.
+    f) Path - Connection of nodes excluding the leaf node.
+    g) Branch - Connection of nodes including the leaf node.
+    h) Height/Depth of the tree - No. of the nodes in the largest branch OR Total no. of generations.
+'''
