86c640a5ef423052bb7763c73c15e8a3ec184fb2
Dhanisha <dhanisha522292@gmail.com>
2025-09-10 22:13:31 +0200

    BT

--- Tree.py
+++ Tree.py
@@ -24,11 +24,18 @@ ordered pair of disjoint binary trees T1 and T2.
 
 --> Any node in the Binary tree has 0,1,or 2 child nodes.
 
---> types of Binary Tree: a) Complete Binary Tree
-                          b) Almost Complete Binary Tree
-                          c) Strict Binary Tree
+--> types of Binary Tree: a) Complete Binary Tree - All levels are completely filled.
+                          b) Almost Complete Binary Tree - All levels are completely filled except the last level
+                             and the nodes in the last level are left aligned.
+                          c) Strict Binary Tree - Each node in this type of tree have only 0 or 2 children nodes.
 
 --> Representation of a Binary Tree: a) Array Representation
                                      b) Linked Representation (by default)
 
+--> We can't do implementation of Binary Tree because for every developer the insertion, deletion and other
+    implementation methods can differ so for unique solutions, we work on specific versions of Binary Tree.
+--> One of the version of Binary Tree is "Binary Search Tree".
+
+
+Binary Search Tree
 '''
