8df4b55a8eb4f53d122d2edc85e8bafcabd8abef
Dhanisha <dhanisha522292@gmail.com>
2025-09-12 03:18:21 +0200

    BST

--- Tree.py
+++ Tree.py
@@ -62,6 +62,16 @@ with the average running time with complexity f(n) = O(log2n)
 
 #Implementation of Binary Search Tree
 
+'''
+--> Define a class Node. The item is used to hold the data,left and right var are used to refer the child nodes.
+--> Define a class BST. Make init method to create root var to hold the ref of the root node.
+--> Define an insert method to store new data in BST.
+--> Define a search method to find a particular item in the BST and return None if the search failed.
+--> Define a method to implement inorder traversing in BST.
+--> Define a method to implement preorder traversing in BST.
+--> Define a method to implement postorder traversing in BST.
+'''
+
 class Node:
     def __init__(self,item=None,left=None,right=None):
         self.item=item
@@ -71,5 +81,56 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root=None
+
     def insert(self,data):
+        self.root=self.rinsert(self.root,data)  
+    def rinsert(self,root,data):  #root of subtree
+        if root is None:
+            return Node(data)
+        elif data<root.item:
+            root.left=self.rinsert(root.left,data)
+        elif data>root.item:
+            root.right=self.rinsert(root.right,data)
+        return root
+    
+    def search(self,data):
+        return self.rsearch(self.root,data) 
+    def rsearch(self,root,data): #subtree root
+        if root is None or root.item==data:  #to match a data and item
+            return root
+        if data<root.item:
+            return self.rsearch(root.left,data) #to move in left subtree
+        else:
+            return self.rsearch(root.right,data) #to move in right subtree
+        
+    def inorder(self):
+        result=[]
+        self.rinorder(self.root,result)
+        return result
+    def rinorder(self,root,result):
+        if root:  #only runs this if not None
+            self.rinorder(root.left,result)
+            result.append(root.item)
+            self.rinorder(root.right,result)
+
+    def preorder(self):
+        result=[]
+        self.rpreorder(self.root,result)
+        return result
+    def rpreorder(self,root,result):
+        if root:  #only runs this if not None
+            result.append(root.item)
+            self.rpreorder(root.left,result)
+            self.rpreorder(root.right,result)
+
+    def postorder(self):
+        result=[]
+        self.rpostorder(self.root,result)
+        return result
+    def rpostorder(self,root,result):
+        if root:  #only runs this if not None
+            self.rpostorder(root.left,result)
+            self.rpostorder(root.right,result)
+            result.append(root.item)
+
         
