237ce1cec43a41d2eee73027be4c567c3b398123
Dhanisha <dhanisha522292@gmail.com>
2025-09-16 03:32:22 +0200

    graph using list

--- Graph.py
+++ Graph.py
@@ -55,3 +55,57 @@ print("\nCheck edges:")
 print("Has edge (0,1):", g.has_edge(0, 1))
 print("Has edge (0,4):", g.has_edge(0, 4))
 
+#2. Implementation of Graph using Adjacency List
+
+'''
+--> Define a class Graph to implement the representation of graph using adjacency list.
+--> Define the init method to initialise the instance object variable vertex_count and dictionary
+    adj_list where key is the vertex number and value is the list of adjacent vertices.
+--> Define add_edge method to add an edge in the graph with the given vertices and the weights.
+--> Define remove_edge method to remove an edge from the graph.
+--> Define has_edge method to check weather an edge exists or not for a given pair of vertices.
+--> Define print adj_list method to print the adjacency list of the graph.
+'''
+
+class Graph_list:
+    def __init__(self,vno):
+        self.vertex_count=vno
+        self.adj_list={v:[] for v in range(vno)}
+
+    def add_edge(self,u,v,weight=1):
+        if 0<=u<self.vertex_count and 0<=v<self.vertex_count:
+            self.adj_list[u].append((v,weight))
+            self.adj_list[v].append((u,weight))
+        else:
+            print("Invalid vertex")
+
+    def remove_edge(self,u,v):
+        if 0<=u<self.vertex_count and 0<=v<self.vertex_count:
+            self.adj_list[u]=[(vertex,weight) for vertex,weight in self.adj_list[u] if vertex!=v]
+            self.adj_list[v]=[(vertex,weight) for vertex,weight in self.adj_list[v] if vertex!=u]
+        else:
+            print("Invalid Vertex")
+
+    def has_edge(self,u,v):
+        if 0<=u<self.vertex_count and 0<=v<self.vertex_count:
+            return any(vertex==v for vertex,weight in self.adj_list[u])
+        else:
+            print("Invalid Vertex")
+
+    def print_adj_list(self):
+        for vertex,n in self.adj_list.items():
+            print("V",vertex,":",n)
+
+
+
+g2=Graph_list(5)
+g2.add_edge(0,1)
+g2.add_edge(1,2)
+g2.add_edge(1,3)
+g2.add_edge(2,4)
+g2.add_edge(3,4)
+g2.print_adj_list()
+
+print("\nCheck edges:")
+print("Has edge (0,1):", g2.has_edge(0, 1))
+print("Has edge (0,4):", g2.has_edge(0, 4))
