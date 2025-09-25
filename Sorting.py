f9905139bcb51f21d277b976d64291ee4372b622
Dhanisha <dhanisha522292@gmail.com>
2025-09-25 17:31:21 +0200

    insertion sort

--- Sorting.py
+++ Sorting.py
@@ -50,3 +50,21 @@ def selection_sort(list1):
 l=[22,67,48,13,89,54,21]
 selection_sort(l)
 print(l)
+
+
+
+#Implementation of Insertion sort
+
+def insertion_sort(list1):
+    for i in range(1,len(list1)):
+        temp=list1[i]
+        
+        j=i-1
+        while j>=0 and temp<list1[j]:
+            list1[j+1] = list1[j]
+            j-=1
+        list1[j+1] = temp
+
+l = [22,67,48,13,89,54,21]
+insertion_sort(l)
+print(l)
