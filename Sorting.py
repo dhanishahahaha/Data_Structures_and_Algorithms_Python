24945c8a940cefc220f26efab0592e700a2e66c1
Dhanisha <dhanisha522292@gmail.com>
2025-09-25 16:39:50 +0200

    selection sort

--- Sorting.py
+++ Sorting.py
@@ -3,6 +3,8 @@
 '''
 --> Write a python code to implement bubble sorting.
 --> Write a python code to implement modified bubble sort.
+--> Write a python code to implement the selection sort.
+--> Write a python code to implement the insertion sort.
 '''
 
 #Implementation of Bubble sort
@@ -33,3 +35,18 @@ l=[34,67,12,89,25,50]
 modified_bubble_sort(l)
 print(l)
     
+
+#Implementation of the selection sort
+
+def selection_sort(list1):
+    n = len(list1)
+    for i in range(n):  #loop on all elements of list1
+        min_index=i #index of the smallest element
+        for j in range(i+1,n): #comparision of all elements with i, to find if there exists a smaller element than i
+            if list1[j]<list1[min_index]: #if another index j is smaller than i
+                min_index=j #then update min_index with j
+        list1[i], list1[min_index] = list1[min_index], list1[i] #swap i with the smallest index found 
+
+l=[22,67,48,13,89,54,21]
+selection_sort(l)
+print(l)
